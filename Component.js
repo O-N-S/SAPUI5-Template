d5ba1b8b2547004f867581e80ccce1be6cab8937
O/N/S - Ozan Nurettin Süel <ozan.nurettin.sueel@gmail.com>
2016-08-31 11:09:21 +0200

    Add files via upload
    Initialize

---
+++ Component.js
@@ -0,0 +1,8 @@
+sap.ui.define(['sap/ui/core/UIComponent','sap/ui/model/json/JSONModel'], function (UIComponent,JSONModel) { "use strict"; return UIComponent.extend("ozan.ons.Component", {
+					
+		metadata: { manifest: "json" },
+		init: function () {
+			UIComponent.prototype.init.apply(this, arguments);
+			this.getRouter().initialize();
+		}
+	}); });
